f116500ac9236a80fdfa4f92b76750b7d9b2b7c0
Mickael Melkowski <m.melkowski@yahoo.fr>
2019-07-29 11:36:57 +0200

    initial commit on R shiny project

---
+++ Iris_Interact/app.R
@@ -0,0 +1,57 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(datasets)
+library(dplyr)
+
+# Define UI
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Iris Dataset Interact"),
+
+    
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("feature_1",
+                        "Feature 1:",
+                        choices = names(iris)),
+            selectInput("feature_2",
+                        "Feature 2:",
+                        choices = names(iris)),
+            
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        
+        plot(iris %>% select(input$feature_1, input$feature_2),
+             bg = c("red", "green3", "blue")[unclass(iris$Species)])
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
